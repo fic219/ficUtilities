9e541c12a1cf3696b988db173a1638c8dd49eea4
Csengeri Máté <mate.csengeri@sanomamedia.hu>
2015-06-11 11:28:55 +0200

    Add public header to scheme.

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char UtilsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Utils/PublicHeader.h>
 
+#import <Utils/FUFonts.h>
 
