7c38322cd9ef919dbae4a1ad182c77a613bf96ef
Csengeri Máté <mate.csengeri@sanomamedia.hu>
2015-06-11 11:02:10 +0200

    Initial commit.

---
+++ ficUtilities/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ficUtilities
+//
+//  Created by Csengeri Máté on 11/06/15.
+//  Copyright (c) 2015 Sanoma MDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
